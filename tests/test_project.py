4a0cc632076d5c96a92ade3aa903f32bd60c715b
zenith0 <pernstef@gmail.com>
2024-01-12 19:49:08 +0100

    Update test_project.py
    added tests for:
    - create_env
    - write_main_py

--- tests/test_project.py
+++ tests/test_project.py
@@ -79,8 +79,32 @@ class TestFolderStructure(unittest.TestCase):
 
         # Check if gitignore is empty
         self.assertTrue(len(gitignore_contents) > 0)
-        
-         
+    
+    def test_write_main_py(self):
+        prj = Project("test_project", self.project_path)
+        prj.initialize_folder_file_structure()
+        prj.write_main_py()
+        # This file exists in the proj directory
+        main_py_path = os.path.join(self.project_path, "test_project", "test_project", "main.py")
+        with open(main_py_path, "r") as file:
+            content = file.read()        
+        # Check if the venv marker file exists in the current directory
+        self.assertTrue (len(content) > 0)
+  
+    def test_create_venv(self):
+        prj = Project("test_project", self.project_path)
+        prj.initialize_folder_file_structure()
+        prj.create_venv()
+        """
+        Check if a virtual environment (venv) is available in the current directory.
+
+        Returns:
+        - bool: True if venv is available, False otherwise.
+        """
+        venv_marker_file = os.path.join(self.project_path, "test_project", "pyvenv.cfg")  # This file exists in the venv directory
+
+        # Check if the venv marker file exists in the current directory
+        self.assertTrue(os.path.exists(venv_marker_file))
         
 if __name__ == '__main__':
     unittest.main()
